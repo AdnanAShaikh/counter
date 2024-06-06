e9bf528ffbd93a8f583410e20535ef112b864ae9
AdnanAShaikh <shaikhadnan862@gmail.com>
2024-06-06 20:18:04 +0530

    reset

--- src/App.js
+++ src/App.js
@@ -10,25 +10,33 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function handleSubtract() {
-    if (count > 0) {
-      setCount((count) => count - 1);
-    }
+    setCount((count) => count - 1);
   }
 
   function handleAdd() {
     setCount((count) => count + 1);
   }
 
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
-    <div className="container">
-      <button onClick={handleSubtract} className="button">
-        -
+    <>
+      <div className="container">
+        <button onClick={handleSubtract} className="button">
+          -
+        </button>
+        <h3 className="count">{count}</h3>
+
+        <button onClick={handleAdd} className="button">
+          +
+        </button>
+      </div>
+      <button className="button reset" onClick={handleReset}>
+        reset
       </button>
-      <h3 className="count">{count}</h3>
-      <button onClick={handleAdd} className="button">
-        +
-      </button>
-    </div>
+    </>
   );
 }
 
